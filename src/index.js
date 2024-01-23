4cd53eb9aedbcbb492cab96f69a37570984b331b
Tyui70 <118425755+Tyui70@users.noreply.github.com>
2024-01-23 15:20:55 +0100

    Update index.js
    Modification for the issue "Add Options After Conversion for Further Actions or Exit" : ask for the user if he wants to continue or exit each time he converts a text

--- src/index.js
+++ src/index.js
@@ -6,29 +6,36 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Veuillez saisir le texte à convertir en Ascii art: ', function(text) {
-    console.log("Choisissez un style : 1. Doom, 2. Rusted");
-    rl.question('Entrez le numéro du style: ', function(number) {
-        let style;
-        switch (number) {
-            case '1':
-                style = 'Doom';
-                break;
-            case '2':
-                style = 'rusted';
-                break;
-            default:
-                style = 'Doom';
-                break;
-        }
-        generateAsciiArt(text, style);
-        rl.close();
-    });
-});
-
 function generateAsciiArt(text, style) {
     AsciiArt.font(text, style, function(err, rendered) {
         if (err) throw err;
         console.log(rendered);
     });
 }
+
+a = True;
+
+while(a == True)
+  {
+    rl.question('Veuillez saisir le texte à convertir en Ascii art: ', function(text) {
+        console.log("Choisissez un style : 1. Doom, 2. Rusted");
+        rl.question('Entrez le numéro du style: ', function(number) {
+            let style;
+            switch (number) {
+                case '1':
+                    style = 'Doom';
+                    break;
+                case '2':
+                    style = 'rusted';
+                    break;
+                default:
+                    style = 'Doom';
+                    break;
+            }
+            generateAsciiArt(text, style);
+            rl.close();
+        });
+    });
+    rl.question("Tapez <oui> pour continuer ou autre chose pour sortir.\n", function(c))
+    if(c != "oui"){a = False)
+  }
